1d8cc3ba0de1da76f3693d96cff76981398a99b4
Steven Biggs <Biggs.Steven.M@gmail.com>
2013-12-15 03:04:57 -0500

    Working with the Item class
    Added a test Item and edited the list box. Actually separating it into
    three list box (quantity, name, price)

--- Dandelion/SalesForm.Designer.vb
+++ Dandelion/SalesForm.Designer.vb
@@ -25,11 +25,8 @@ Partial Class SalesForm
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SalesForm))
         Me.CartGrpBox = New System.Windows.Forms.GroupBox()
         Me.BtnCntrlGrpBox = New System.Windows.Forms.GroupBox()
-        Me.CartLstBox = New System.Windows.Forms.ListBox()
         Me.CatagoryTabCntrl = New System.Windows.Forms.TabControl()
         Me.ColdFoodTabPage = New System.Windows.Forms.TabPage()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.Rob_ShopDataSet1 = New Dandelion.Rob_ShopDataSet()
         Me.CookedTabPage = New System.Windows.Forms.TabPage()
         Me.BeverageTabPage = New System.Windows.Forms.TabPage()
         Me.AlcoholTabPage = New System.Windows.Forms.TabPage()
@@ -40,18 +37,20 @@ Partial Class SalesForm
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SearchTxtBox = New System.Windows.Forms.TextBox()
+        Me.quanLstBox = New System.Windows.Forms.ListBox()
+        Me.nameLstBox = New System.Windows.Forms.ListBox()
+        Me.priceListBox = New System.Windows.Forms.ListBox()
         Me.CartGrpBox.SuspendLayout()
         Me.CatagoryTabCntrl.SuspendLayout()
-        Me.ColdFoodTabPage.SuspendLayout()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.Rob_ShopDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SalesMnuStrp.SuspendLayout()
         Me.SuspendLayout()
         '
         'CartGrpBox
         '
+        Me.CartGrpBox.Controls.Add(Me.priceListBox)
+        Me.CartGrpBox.Controls.Add(Me.nameLstBox)
+        Me.CartGrpBox.Controls.Add(Me.quanLstBox)
         Me.CartGrpBox.Controls.Add(Me.BtnCntrlGrpBox)
-        Me.CartGrpBox.Controls.Add(Me.CartLstBox)
         Me.CartGrpBox.Dock = System.Windows.Forms.DockStyle.Right
         Me.CartGrpBox.Location = New System.Drawing.Point(1150, 24)
         Me.CartGrpBox.Name = "CartGrpBox"
@@ -68,15 +67,6 @@ Partial Class SalesForm
         Me.BtnCntrlGrpBox.Size = New System.Drawing.Size(260, 298)
         Me.BtnCntrlGrpBox.TabIndex = 3
         Me.BtnCntrlGrpBox.TabStop = False
-        '
-        'CartLstBox
-        '
-        Me.CartLstBox.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.CartLstBox.FormattingEnabled = True
-        Me.CartLstBox.Location = New System.Drawing.Point(3, 16)
-        Me.CartLstBox.Name = "CartLstBox"
-        Me.CartLstBox.Size = New System.Drawing.Size(260, 813)
-        Me.CartLstBox.TabIndex = 1
         '
         'CatagoryTabCntrl
         '
@@ -100,28 +90,11 @@ Partial Class SalesForm
         Me.ColdFoodTabPage.BackColor = System.Drawing.Color.Transparent
         Me.ColdFoodTabPage.BackgroundImage = Global.Dandelion.My.Resources.Resources.Dandelion_logo_background
         Me.ColdFoodTabPage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ColdFoodTabPage.Controls.Add(Me.DataGridView1)
         Me.ColdFoodTabPage.Location = New System.Drawing.Point(4, 22)
         Me.ColdFoodTabPage.Name = "ColdFoodTabPage"
         Me.ColdFoodTabPage.Size = New System.Drawing.Size(1142, 806)
         Me.ColdFoodTabPage.TabIndex = 3
         Me.ColdFoodTabPage.Text = "Cold Food"
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.AutoGenerateColumns = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.DataSource = Me.Rob_ShopDataSet1
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(1142, 806)
-        Me.DataGridView1.TabIndex = 0
-        '
-        'Rob_ShopDataSet1
-        '
-        Me.Rob_ShopDataSet1.DataSetName = "Rob_ShopDataSet"
-        Me.Rob_ShopDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
         '
         'CookedTabPage
         '
@@ -221,6 +194,33 @@ Partial Class SalesForm
         Me.SearchTxtBox.Size = New System.Drawing.Size(155, 20)
         Me.SearchTxtBox.TabIndex = 2
         '
+        'quanLstBox
+        '
+        Me.quanLstBox.Dock = System.Windows.Forms.DockStyle.Left
+        Me.quanLstBox.FormattingEnabled = True
+        Me.quanLstBox.Location = New System.Drawing.Point(3, 16)
+        Me.quanLstBox.Name = "quanLstBox"
+        Me.quanLstBox.Size = New System.Drawing.Size(34, 515)
+        Me.quanLstBox.TabIndex = 4
+        '
+        'nameLstBox
+        '
+        Me.nameLstBox.Dock = System.Windows.Forms.DockStyle.Left
+        Me.nameLstBox.FormattingEnabled = True
+        Me.nameLstBox.Location = New System.Drawing.Point(37, 16)
+        Me.nameLstBox.Name = "nameLstBox"
+        Me.nameLstBox.Size = New System.Drawing.Size(172, 515)
+        Me.nameLstBox.TabIndex = 5
+        '
+        'priceListBox
+        '
+        Me.priceListBox.Dock = System.Windows.Forms.DockStyle.Left
+        Me.priceListBox.FormattingEnabled = True
+        Me.priceListBox.Location = New System.Drawing.Point(209, 16)
+        Me.priceListBox.Name = "priceListBox"
+        Me.priceListBox.Size = New System.Drawing.Size(58, 515)
+        Me.priceListBox.TabIndex = 6
+        '
         'SalesForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -238,9 +238,6 @@ Partial Class SalesForm
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.CartGrpBox.ResumeLayout(False)
         Me.CatagoryTabCntrl.ResumeLayout(False)
-        Me.ColdFoodTabPage.ResumeLayout(False)
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.Rob_ShopDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.SalesMnuStrp.ResumeLayout(False)
         Me.SalesMnuStrp.PerformLayout()
         Me.ResumeLayout(False)
@@ -248,7 +245,6 @@ Partial Class SalesForm
 
     End Sub
     Friend WithEvents CartGrpBox As System.Windows.Forms.GroupBox
-    Friend WithEvents CartLstBox As System.Windows.Forms.ListBox
     Friend WithEvents CatagoryTabCntrl As System.Windows.Forms.TabControl
     Friend WithEvents ColdFoodTabPage As System.Windows.Forms.TabPage
     Friend WithEvents CookedTabPage As System.Windows.Forms.TabPage
@@ -262,7 +258,8 @@ Partial Class SalesForm
     Friend WithEvents SettingsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents BtnCntrlGrpBox As System.Windows.Forms.GroupBox
     Friend WithEvents SearchTxtBox As System.Windows.Forms.TextBox
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents Rob_ShopDataSet1 As Dandelion.Rob_ShopDataSet
+    Friend WithEvents priceListBox As System.Windows.Forms.ListBox
+    Friend WithEvents nameLstBox As System.Windows.Forms.ListBox
+    Friend WithEvents quanLstBox As System.Windows.Forms.ListBox
 
 End Class
--- Dandelion/SalesForm.vb
+++ Dandelion/SalesForm.vb
@@ -3,11 +3,8 @@ Public Class SalesForm
 
     Private Sub SalesForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'TODO: This line of code loads data into the 'DandelionDB1DataSet11.Item' table. You can move, or remove it, as needed.
-
-
-
+        Dim testItem As New Item
+        testItem.setAll("343311", "Redds Strawberry", "Alcoholic", 2.99)
 
     End Sub
-
-
 End Class
